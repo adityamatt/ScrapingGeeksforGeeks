af03005ad9988131d43843614f2a3bf65060ea08
adityamatt <aditya.tiwarics@gmail.com>
2018-05-20 23:10:44 +0530

    Updated Program,removed bugs,old had filename saved with traliing \n removed that

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from openpyxl import Workbook
+from openpyxl import load_workbook
 import urllib2
 from django.utils.encoding import smart_str, smart_unicode
 from bs4 import BeautifulSoup
+from pathlib import Path
+import sys
 
 #fileName="GeeksforGeeks.xlsx"
 #SheetName="Arrays"
@@ -12,17 +15,10 @@ with open('./setting.properties') as fp:
     quote_page=str(fp.readline())
     fileName=str(fp.readline())
     SheetName=str(fp.readline())
-quote_page.replace("\n","")
-fileName.replace("\n","")
-SheetName.replace("\n","")
+quote_page=quote_page.rstrip('\n')
+fileName=fileName.rstrip('\n')
+SheetName=SheetName.rstrip('\n')
 
-print quote_page
-print fileName
-print SheetName
-
-print "https://www.geeksforgeeks.org/array-data-structure/"
-print "Arrays"
-print "GeeksforGeeks.xlsx"
 
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
@@ -31,9 +27,19 @@ li_list=soup.find_all('li')
 pattern='"https://www.geeksforgeeks.org/"[A-Za-z]+'
 
 count=1
-wb = Workbook()
-ws = wb.create_sheet(SheetName)
-ws.title = SheetName
+
+my_file = Path(fileName)
+if my_file.is_file():
+    wb = load_workbook(filename = fileName)
+    ws = wb.create_sheet(SheetName)
+    ws.title = SheetName
+#    print "file already exists"
+#    sys.exit(0)
+    
+else:
+    wb = Workbook()
+    ws = wb.create_sheet(SheetName)
+    ws.title = SheetName
 
 for something in li_list:
     for anchor_tag in something.find_all('a',href=True):
@@ -43,8 +49,10 @@ for something in li_list:
         text=smart_str(anchor_tag.string)
         if text.count(' ')<=3:
             continue
+        new_link="=HYPERLINK(\""+link+"\",\"link\")"
         ws.cell(row=count,column=1).value=text;
         ws.cell(row=count,column=2).value=link;
+        ws.cell(row=count,column=3).value=new_link;
         count=count+1
 
 wb.save(fileName)
